8cff023775e4853402e9044f88ad3020c894ef35
Paul Keahi <paulkeahi@gmail.com>
2015-10-10 11:25:15 -1000

    twitter and instagram access

---
+++ server/_settings.js
@@ -0,0 +1,29 @@
+if (typeof Meteor.settings === 'undefined')
+  Meteor.settings = {};
+
+_.defaults(Meteor.settings, {
+  twitter: {
+    consumerKey: "uJbwaikF3EVYg3qObJkKrSsTJ",
+    secret: "zDRdHaonjAr7r7DssDIq3AVOMwAgd22bAu5uKVuGAEdksgc9kI"
+  }
+});
+
+ServiceConfiguration.configurations.upsert(
+  { service: "twitter" },
+  {
+    $set: {
+      consumerKey: Meteor.settings.twitter.consumerKey,
+      secret: Meteor.settings.twitter.secret
+    }
+  }
+);
+
+ServiceConfiguration.configurations.upsert(
+  { service: "instagram" },
+  {
+    $set: {
+      clientID: "e56af2319b304822a584c0288632f447",
+      secret: "4b9ec825088449a9873f84f4c4f77014"
+    }
+  }
+);
