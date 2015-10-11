75eeb4def02b8e85d8ff3d2cefed04e0e14fc896
Julia Bond <julia@yogaed.com>
2015-10-10 15:48:58 -1000

    minor changes.

--- server/publications.js
+++ server/publications.js
@@ -1,7 +1,7 @@
 Meteor.publish('instaSearch', function(query){
   var self = this;
   try {
-    var response = HTTP.get('https://api.instagram.com/v1/tags/' + query + '/media/recent?access_token=26446004.1677ed0.9f12cafae2e341de9d6ed12572ce428d');
+    var response = HTTP.get('https://api.instagram.com/v1/tags/' + query + '/media/recent?access_token=ACCESS_TOKEN');
     _.each(response.data, function(item){
       console.log(item);
       var data = item.data;
