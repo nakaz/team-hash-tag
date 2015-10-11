07d06ee959aedd958869a173345e0d84935e3c3d
Paul Keahi <Keahibono@users.noreply.github.com>
2015-10-10 19:10:23 -1000

    Update _settings.js

--- server/_settings.js
+++ server/_settings.js
@@ -8,8 +8,8 @@
 
       Accounts.loginServiceConfiguration.insert({
         service     : 'twitter',
-        consumerKey : 'uJbwaikF3EVYg3qObJkKrSsTJ',
-        secret      : 'zDRdHaonjAr7r7DssDIq3AVOMwAgd22bAu5uKVuGAEdksgc9kI'
+        consumerKey : 'XXXXXX',
+        secret      : 'XXXXX'
       });
 
     });
