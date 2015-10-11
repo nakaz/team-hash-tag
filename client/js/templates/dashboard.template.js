8fa286ee19d0470c315a1e16b546d1b7aa61e3ba
Sean Nakamura <seanenakamura@gmail.com>
2015-10-10 15:38:50 -1000

    added js organization

---
+++ client/js/templates/dashboard.template.js
@@ -0,0 +1,19 @@
+Template.dashboard.created = function (){
+  console.log('dashboard created');
+};
+
+Template.dashboard.rendered = function (){
+  console.log('dashboard rendered');
+};
+
+Template.dashboard.destroyed = function (){
+  console.log('dashboard destroyed');
+};
+
+Template.dashboard.helpers({
+
+});
+
+Template.dashboard.events({
+
+});
